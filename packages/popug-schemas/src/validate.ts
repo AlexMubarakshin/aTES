42cd7858147ea623363ca67327fea536bc1693d3
Alex Mubarakshin <muban96@mail.ru>
2023-08-19 17:32:58 +0300

    Update schema registry package
    - Remove dynamic scanning

--- packages/popug-schemas/src/validate.ts
+++ packages/popug-schemas/src/validate.ts
@@ -1,24 +1,41 @@
-import {ValidationResult as JoiValidationResult} from "joi";
+import * as console from "console";
+import {ObjectSchema, ValidationResult as JoiValidationResult} from "joi";
 import {BUSINESS_EVENT, CUD_EVENT, IBrokerEvent, IBusinessEventsValues, ICudEventsValues} from "./constants";
 
-const EVENT_NAME_TO_SCHEMA_PATH: Record<ICudEventsValues | IBusinessEventsValues, string> = {
-  [CUD_EVENT.USER_CREATED]: 'users/created',
-  [CUD_EVENT.USER_DELETED]: 'users/deleted',
-  [CUD_EVENT.TASK_UPDATED]: 'tasks/updated',
-  [BUSINESS_EVENT.USER_ROLE_CHANGED]: 'users/role-changed',
-  [BUSINESS_EVENT.TASK_ADDED]: 'tasks/added',
-  [BUSINESS_EVENT.TASK_COMPLETED]: 'tasks/completed',
-  [BUSINESS_EVENT.TASKS_SHUFFLE_STARTED]: 'tasks/shuffle-started',
+import UsersCreatedV1 from './users/created-v1'
+import UsersDeletedV1 from './users/deleted-v1'
+import TasksUpdatedV1 from './tasks/updated-v1'
+import TasksUpdatedV2 from './tasks/updated-v2'
+import UsersRoleChangedV1 from './users/role-changed-v1'
+import TasksAddedV1 from './tasks/added-v1'
+import TasksAddedV2 from './tasks/added-v2'
+import TasksCompletedV1 from './tasks/completed-v1'
+import TasksShuffleStartedV1 from './tasks/shuffle-started-v1'
+
+const EVENT_NAME_TO_SCHEMA_PATH: Record<ICudEventsValues | IBusinessEventsValues, Array<{
+  version: number;
+  schema: ObjectSchema
+}>> = {
+  [CUD_EVENT.USER_CREATED]: [{schema: UsersCreatedV1, version: 1},],
+  [CUD_EVENT.USER_DELETED]: [{schema: UsersDeletedV1, version: 1},],
+  [CUD_EVENT.TASK_UPDATED]: [{schema: TasksUpdatedV1, version: 1}, {schema: TasksUpdatedV2, version: 2}],
+  [BUSINESS_EVENT.USER_ROLE_CHANGED]: [{schema: UsersRoleChangedV1, version: 1},],
+  [BUSINESS_EVENT.TASK_ADDED]: [{schema: TasksAddedV1, version: 1}, {schema: TasksAddedV2, version: 2}],
+  [BUSINESS_EVENT.TASK_COMPLETED]: [{schema: TasksCompletedV1, version: 1},],
+  [BUSINESS_EVENT.TASKS_SHUFFLE_STARTED]: [{schema: TasksShuffleStartedV1, version: 1},],
 };
 
 export function validateEvent(event: IBrokerEvent) {
-  const path = EVENT_NAME_TO_SCHEMA_PATH[event.type];
-  if (!path) return {isValid: false, error: {type: `Missing schema for event with type ${event.type}`}};
+  const schemas = EVENT_NAME_TO_SCHEMA_PATH[event.type];
+  const schema = schemas?.find(s => s.version === event.version)
+  if (!schema) return {
+    isValid: false,
+    error: {type: `Missing schema for event with type ${event.type} or version ${event.version}`}
+  };
 
   try {
-    const schema = require(`./shemas/${path}-${event.version}`);
 
-    const result: JoiValidationResult = schema.validate(event, {abortEarly: false});
+    const result: JoiValidationResult = schema.schema.validate(event, {abortEarly: false});
 
     if (result.error) {
       const error = result.error.details.map((e) => ({[e.path.join('.')]: e.message}));
@@ -27,6 +44,7 @@ export function validateEvent(event: IBrokerEvent) {
 
     return {isValid: true, error: null};
   } catch (error) {
-    return {isValid: false, error: {version: `No schema for event with version ${event.vestion}`}};
+    console.error(error)
+    return {isValid: false, error: {version: `Unknown error for event ${event.type} v${event.version}`}};
   }
 }
